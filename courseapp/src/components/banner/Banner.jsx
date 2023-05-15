4e4c187e06e5685b8f9967cc49b99b7ad20e9a78
saidsn <saidsn@code.edu.az>
2023-05-15 18:20:44 +0400

    Fixed: Banner Component

--- courseapp/src/components/banner/Banner.jsx
+++ courseapp/src/components/banner/Banner.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
 
+
 function Banner() {
+
+    const url = 'https://localhost:7184';
+
+    const [banner, setBanner] = useState([]);
+
+    const getAllBanner = async () => {
+        await axios.get(`${url}/api/Banner/GetAll`)
+            .then((res) => {
+                setBanner(res.data);
+            });
+    }
+
+    useEffect(() => {
+        getAllBanner();
+    }, []);
+
+
     return (
 
         <div className="hero_boxes">
             <div className="hero_boxes_inner">
                 <div className="container" style={{ maxWidth: "1140px" }}>
                     <div className="row">
-                        <div className="col-lg-4 hero_box_col" data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom" data-aos-duration="700">
-                            <div className="hero_box d-flex flex-row align-items-center justify-content-start">
-                                <img className='svg' src="images/earth-globe.svg" alt="" />
-                                <div className="hero_box_content">
-                                    <h2 className="hero_box_title">Online Courses</h2>
-                                    <NavLink className='hero_box_link' to="/courses">View More</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 hero_box_col" data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom" data-aos-duration="1200">
-                            <div className="hero_box d-flex flex-row align-items-center justify-content-start">
-                                <img className='svg' src="images/books.svg" alt="" />
-                                <div className="hero_box_content">
-                                    <h2 className="hero_box_title">Our Library</h2>
-                                    <NavLink className='hero_box_link' to="https://github.com/saidsn">View More</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 hero_box_col" data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom" data-aos-duration="1600">
-                            <div className="hero_box d-flex flex-row align-items-center justify-content-start">
-                                <img className='svg' src="images/professor.svg" alt="" />
-                                <div className="hero_box_content">
-                                    <h2 className="hero_box_title">Our Teachers</h2>
-                                    <NavLink className='hero_box_link' to="https://github.com/saidsn">View More</NavLink>
-                                </div>
-                            </div>
-                        </div>
+                        {
+                            banner.map((banner, index) => {
+                                return (
+                                    <div key={index} className="col-lg-4 hero_box_col" data-aos="fade-up"
+                                        data-aos-anchor-placement="bottom-bottom" data-aos-duration="700">
+                                        <div className="hero_box d-flex flex-row align-items-center justify-content-start">
+                                            <img className='svg' src={`data:image/svg+xml;base64, ${banner.image}`} alt="" />
+                                            <div className="hero_box_content">
+                                                <h2 className="hero_box_title">{banner.title}</h2>
+                                                <NavLink className='hero_box_link' to="/courses">View More</NavLink>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>

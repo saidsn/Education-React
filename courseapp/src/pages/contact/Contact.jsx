426f5280b0252bdd95deefc1a6f44e14821bd415
saidsn <saidsn@code.edu.az>
2023-06-02 19:36:39 +0400

    Feature: Contact Create Method

--- courseapp/src/pages/contact/Contact.jsx
+++ courseapp/src/pages/contact/Contact.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import Navbar from '../../components/layout/Navbar';
 import '../../../src/assets/styles/Contact.css'
 import Header from '../../components/header/Header'
@@ -9,6 +11,35 @@ import 'aos/dist/aos.css';
 AOS.init();
 
 function Contact() {
+
+    const url = 'https://localhost:7184';
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const submitMessage = async (e) => {
+        e.preventDefault();
+
+        const newMessage = {
+            name,
+            email,
+            message
+        }
+        await axios
+            .post(`${url}/api/Contact/Create`, newMessage)
+            .then((res) => {
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+
+        setName("");
+        setEmail("");
+        setMessage("");
+    }
+
     return (
         <>
             <Navbar />
@@ -16,24 +47,45 @@ function Contact() {
             <div className="contact">
                 <div className="container" style={{ maxWidth: "1140px" }}>
                     <div className="row">
-                        <div className="col-lg-8">
+                        <div className="col-lg-7">
                             <div className="contact_form" data-aos="fade-up" data-aos-duration="800">
                                 <div className="contact_title">Get in touch</div>
                                 <div className="contact_form_container">
-                                    <form >
-                                        <input id="contact_form_name" className="input_field contact_form_name" type="text" placeholder="Name"
-                                            required="required" data-error="Name is required." />
-                                        <input id="contact_form_email" className="input_field contact_form_email" type="email"
-                                            placeholder="E-mail" required="required" data-error="Valid email is required." />
-                                        <textarea id="contact_form_message" className="text_field contact_form_message" name="message"
-                                            placeholder="Message" required="required" data-error="Please, write us a message."></textarea>
-                                        <button id="contact_send_btn" type="button" className="contact_send_btn trans_200" value="Submit">send
+                                    <form onSubmit={submitMessage} >
+                                        <input
+                                            id="contact_form_name"
+                                            className="input_field contact_form_name"
+                                            value={name}
+                                            onChange={(e) => setName(e.target.value)}
+                                            type="text"
+                                            placeholder="Name"
+                                            required="required"
+                                        />
+                                        <input
+                                            id="contact_form_email"
+                                            className="input_field contact_form_email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            type="email"
+                                            placeholder="E-mail"
+                                            required="required"
+                                        />
+                                        <textarea
+                                            id="contact_form_message"
+                                            className="text_field contact_form_message"
+                                            value={message}
+                                            onChange={(e) => setMessage(e.target.value)}
+                                            name="message"
+                                            placeholder="Message"
+                                            required="required"
+                                        ></textarea>
+                                        <button id="contact_send_btn" type="submit" className="contact_send_btn trans_200" value="Submit">send
                                             message</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-4">
+                        <div className="col-lg-5">
                             <div className="about" data-aos="fade-up" data-aos-duration="1200">
                                 <div className="about_title">Join Courses</div>
                                 <p className="about_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl
